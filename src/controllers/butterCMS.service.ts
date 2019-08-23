02b9287e9a568e2cfc3989b9f56571a806fcf94f
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2019-08-23 09:50:53 +0700

    Fix minor import error

--- src/controllers/butterCMS.service.ts
+++ src/controllers/butterCMS.service.ts
@@ -1,4 +1,4 @@
 import * as Butter from 'buttercms';
-import { GLOBAL_CONFIGS } from 'src/configs/global-config';
+import { GLOBAL_CONFIGS } from '../configs/global-config';
 
 export const butterService = Butter(GLOBAL_CONFIGS.GENERAL_CONFIGS.BUTTER_CMS_API_KEY, false, 10000);
